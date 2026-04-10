c61962d1a49912eeee3a9294f9c9549446baefb1
code web <humairat291@gmail.com>
2026-04-10 17:55:09 +0530

    Implement recursive descent parser in C

---
+++ recursiveDescentParsing.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include<string.h>
+char input[100];
+int i=0;
+
+void E();
+void Eprime();
+void T();
+void Tprime();
+void F();
+
+void match(char c){
+    if(input[i] == c)
+    i++;
+    else{
+        rpintf("error in parsing...\n");
+        return;
+    }
+}
+//E-->TE'
+void E(){
+ T();
+ Eprime();
+}
+
+void Eprime(){
+    if(input[i] == '+'){
+    match('+');
+    T();
+    Eprime();
+    }
+}
+
+void T(){
+    F();
+    Tprime();
+}
+
+void Tprime(){
+    if(input[i] == '*'){
+        match(*);
+        F();
+        Tprime();
+    }
+    
+}
+
+void F(){
+    if(input[i] == '('){
+        match('(');
+        E();
+        match(')');
+    }
+    else if(input[i] == 'i' && input[i+1] == 'd'){
+        match('i');
+        match('d');
+    }
+    else{
+        printf("Error in parsing\n");
+        
+    }
+}
+
+
+int main() {
+    printf("Enter input string:");
+    scanf("%s",input);
+    E();
+    
+    if(input[i] == '\0'){
+        printf("successfully parsed\n");
+    }
+    else{
+        printf("error in parsing\n");
+    }
+    
+return 0;
+}
